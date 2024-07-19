5b152059884aca87609e402a8832c1517746b26f
Harsh Bhama <harshbhama96@gmail.com>
2024-07-19 15:13:15 +0530

    added seesing controller ans routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { healthRoutes } from "@gateway/routes/health";
 import { authRoutes } from "./routes/auth";
 import { currentUserRoutes } from "./routes/current-user";
 import { authMiddleware } from "@gateway/services/auth.middleware";
+import { searchRoutes } from "./routes/search";
 
 const BASE_PATH = '/api/gateway/v1';
 
@@ -10,6 +11,7 @@ const BASE_PATH = '/api/gateway/v1';
 export const appRoutes = (app: Application) => {
   app.use('', healthRoutes.routes());
   app.use(BASE_PATH, authRoutes.routes());
+  app.use(BASE_PATH, searchRoutes.routes());
   app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
 }
 
